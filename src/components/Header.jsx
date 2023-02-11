b08d98711b7f4536bc5987a0fbc3c947fce710fb
Priyanshu88064 <priyanshu88064@gmail.com>
2023-02-11 12:34:55 +0530

    added logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 function Header({ rollno }) {
+  const navigate = useNavigate();
+
   return (
     <div className="header">
       <i className="bi bi-list list" />
       <span className="name">{rollno}</span>
-      <i class="bi bi-box-arrow-right"></i>
+      <i
+        class="bi bi-box-arrow-right"
+        onClick={() => {
+          navigate("/");
+        }}
+      ></i>
     </div>
   );
 }
